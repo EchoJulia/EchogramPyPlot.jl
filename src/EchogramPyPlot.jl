e55165f491ffa3403215fcb7affeeb16c931065b
Rob Blackwell <rob@robblackwell.com>
2018-10-21 14:13:53 +0100

    Fixed color problems

--- src/EchogramPyPlot.jl
+++ src/EchogramPyPlot.jl
@@ -1,42 +1,26 @@
 module EchogramPyPlot
 
 using PyPlot
+using ColorTypes
 
 export echogram, eghist, egshow, eg, EK500, BW
 
+const EK500 = [RGB(1, 1, 1),
+                        RGB(159/255, 159/255, 159/255),
+                        RGB(095/255, 095/255, 095/255),
+                        RGB(000, 000, 1),
+                        RGB(000, 000, 127/255),
+                        RGB(000, 191/255, 000),
+                        RGB(000, 127/255, 000),
+                        RGB(1, 1, 000),
+                        RGB(1, 127/255, 000),
+                        RGB(1, 000, 191/255),
+                        RGB(1, 000, 000),
+                        RGB(166/255, 083/255, 060/255),
+                        RGB(120/255, 060/255, 040/255)]
 
-# const EK500 = ColorMap([1 1 1;
-#            159/255 159/255 159/255;
-#            095/255 095/255 095/255;
-#            000 000 1;
-#            000 000 127/255;
-#            000 191/255 000;
-#            000 127/255 000;
-#            1 1 000;
-#            1 127/255 000;
-#            1 000 191/255;
-#            1 000 000;
-#            166/255 083/255 060/255;
-#            120/255 060/255 040/255])
-
-# const BW = ColorMap([0 0 0;1 1 1.0])
-
-const EK500 = ColorMap([(1, 1, 1),
-                        (159/255, 159/255, 159/255),
-                        (095/255, 095/255, 095/255),
-                        (000, 000, 1),
-                        (000, 000, 127/255),
-                        (000, 191/255, 000),
-                        (000, 127/255, 000),
-                        (1, 1, 000),
-                        (1, 127/255, 000),
-                        (1, 000, 191/255),
-                        (1, 000, 000),
-                        (166/255, 083/255, 060/255),
-                        (120/255, 060/255, 040/255)])
-
-const BW = ColorMap([(0,0,0),
-                     (1.0,1.0,1.0)])
+const BW = [RGB(0,0,0),
+                     RGB(1.0,1.0,1.0)]
 
 # echogram is for publication plots and composability, eg, egshow and
 # eghist are for interactive use.
@@ -72,7 +56,7 @@ function echogram(m; vmin = nothing, vmax = nothing, cmap=nothing, range=nothing
     ax[:set_yticks](yticks)
     ax[:set_yticklabels](yticklabels)
     
-    pcolormesh(m, vmin = vmin, vmax = vmax, cmap=cmap)
+    pcolormesh(m, vmin = vmin, vmax = vmax, cmap=ColorMap(cmap))
 end
 
 function eg(A; vmin = nothing, vmax = nothing, cmap=nothing, range=nothing)
